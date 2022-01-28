f7aadea716ab6101618c2896ab456acb1d1a01c9
Oleksii_Ivanov3 <oleksii_ivanov3@epam.com>
2022-01-28 15:53:42 +0200

    adding additional files

---
+++ com.epam.training.student_Oleksii_Ivanov/fundamental/LearnMain.java
@@ -0,0 +1,9 @@
+package com.epam.training.student_Oleksii_Ivanov.fundamental;
+
+public class LearnMain{
+    public static void main(String[ ] args){
+        Double d = Double.POSITIVE_INFINITY;
+        double d1 = d.intValue() + Double.NEGATIVE_INFINITY;
+        System.out.println(d1);
+    }
+}
