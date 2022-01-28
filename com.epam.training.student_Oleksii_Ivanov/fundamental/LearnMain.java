ae6b69820079a9d56999b73e7d6dbc133d5f978f
DeDyLLIka <98542915+DeDyLLIka@users.noreply.github.com>
2022-01-28 15:54:24 +0200

    Update LearnMain.java

--- com.epam.training.student_Oleksii_Ivanov/fundamental/LearnMain.java
+++ com.epam.training.student_Oleksii_Ivanov/fundamental/LearnMain.java
@@ -2,8 +2,7 @@ package com.epam.training.student_Oleksii_Ivanov.fundamental;
 
 public class LearnMain{
     public static void main(String[ ] args){
-        Double d = Double.POSITIVE_INFINITY;
-        double d1 = d.intValue() + Double.NEGATIVE_INFINITY;
+        int d1 = 50;
         System.out.println(d1);
     }
 }
