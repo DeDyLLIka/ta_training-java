8970b3f68967f2f9374e6731dbfad7c920f320fa
Oleksii_Ivanov3 <oleksii_ivanov3@epam.com>
2022-01-31 13:43:23 +0200

    adding additional files

--- com.epam.training.student_Oleksii_Ivanov/fundamental/LearnMain.java
+++ com.epam.training.student_Oleksii_Ivanov/fundamental/LearnMain.java
@@ -1,8 +1,16 @@
 package com.epam.training.student_Oleksii_Ivanov.fundamental;
 
 public class LearnMain{
-    public static void main(String[ ] args){
-        int d1 = 50;
-        System.out.println(d1);
+    public static void main(String args[]){
+        Integer number = 32;
+        // Binary number format
+        String convert = Integer.toBinaryString(number);
+        System.out.print(convert + " ");
+        // Octagonal number format
+        convert = Integer.toOctalString(number);
+        System.out.print(convert + " ");
+        // Hexadecimal number format
+        convert = Integer.toHexString(number).toUpperCase();
+        System.out.print(convert + " ");
     }
 }
